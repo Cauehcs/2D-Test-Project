f4826454460b0af9d503bb5afc000dcd2ca8ae68
Cauehcs <caue.hcs@gmail.com>
2020-07-06 21:33:22 -0300

    Repositório criado

---
+++ Assets/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+
+public float life, speed, forJump;
+
+private static Singleton instance;
+
+
+private void Awake() {
+    
+    instance = this;
+
+}
+
+public static Singleton GetInstance(){
+
+    return instance;
+
+}
+
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+private void Update() {
+    
+    float a = Singleton.GetInstance().speed;
+    Debug.Log(a);
+            
+}
+
+}
